d7e9bc480571804ce09628c301737d4c23af4c72
Faiza <berrichif0717@gmail.com>
2021-06-29 15:47:44 +0200

    ajout partie bonus

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $login="root";
 $pass="";
 
 try{
-    $connexion= new PDO ("mysql:host=$serveur;dbname=webdevelopment",$login,$pass);//j'utilise les bloc try and catch pour la gestion des erreurs
+    $connexion= new PDO ("mysql:host=$serveur;dbname=codex",$login,$pass);//j'utilise les bloc try and catch pour la gestion des erreurs
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //exercice 1
     //$connexion->exec("CREATE DATABASE codex");// j'ai crée une base de donnée codex
@@ -28,8 +28,13 @@ try{
   //$modif="ALTER TABLE `framworks` CHANGE `name` `framwork` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;";
  
     //exercice 6 
-    $codesql="ALTER TABLE `framworks` CHANGE `version` `version` VARCHAR(10) NULL DEFAULT NULL";
-    $connexion->exec($codesql);
+    //$codesql="ALTER TABLE `framworks` CHANGE `version` `version` VARCHAR(10) NULL DEFAULT NULL";
+      //bonus
+      $codesql="ALTER TABLE `clients` DROP `phoneNumber`,
+       CHANGE `email` `mails` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+        ADD zipCode VARCHAR(50),
+        ADD city VARCHAR(50)";
+       $connexion->exec($codesql);
     echo "base de donnée codex créée<br>";
     echo "connexion à la base de donnée codex est reussi<br>";
     echo "ajouter la table clients dans la base de donnée codex avec 7 colonnes<br>";
@@ -37,7 +42,9 @@ try{
     echo "ajouter une colonne de type INT  à la base framworks de la base de donnée webdevelopment <br>";
     echo "changer le nom de la colonne lastName a version dans la table languages<br>";
     echo "changer le nom de la colonne name a framwork dans la table frameworks<br>";
-    echo "changer le type de la colonne version de la table framworks de int a varchar 10";
+    echo "changer le type de la colonne version de la table framworks de int a varchar 10 <br>";
+    echo "connexion à la base de donnée codex reussi<br>";
+    echo "bonus reussi";
 
 
 }catch(PDOException $e){
